a56274c55d184b13c4e0c9173c3468df4a8c743e
muddybarefeet <2muddybarefeet@gmail.com>
2016-07-14 11:09:29 -0700

    add code to present random images from the flickr API

--- SleepingInTheLibrary/ViewController.swift
+++ SleepingInTheLibrary/ViewController.swift
@@ -51,26 +51,58 @@ class ViewController: UIViewController {
             Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
         ]
         
+//        create the request URL
         let urlStr = Constants.Flickr.APIBaseURL + escapedParameters(methodParameters)
         let url = NSURL(string: urlStr)!
         let request = NSURLRequest(URL: url)
         
+//        run the request to get the data from the URL
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
             
+//            if here is no data then continue
             if error == nil {
+//                if ther is a data item
                 if let data = data {
+//                    make a variable placeholder
                     let parsedData: AnyObject!
+                    
+//                    parse the json to readable format
                     do {
 //                        serialization = convert to bytes and de serialization means the opposite all grouped under NSSerialization
                         parsedData = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
                     } catch {
-                        print("Could not parse the data as JSON: '\(data)'")
+//                    throw error if necessary
                         let alert = UIAlertController(title: "Alert", message: "Could not parse the data as JSON: '\(data)'", preferredStyle: UIAlertControllerStyle.Alert)
                         alert.addAction(UIAlertAction(title: "Working!!", style: UIAlertActionStyle.Default, handler: nil))
                         self.presentViewController(alert, animated: true, completion: nil)
                         return
                     }
-                    print(parsedData)
+                    
+//                  if there is a photos key in the response dictionary
+                    if let photosDictionary = parsedData[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject],
+//                        if there is key on the photos dictionary that is "photo" (which is an array of dictionaries)
+                        let photoArray = photosDictionary["photo"] as? [[String: AnyObject]] {
+                        
+//                        make a random index and generate the dictionary at this index
+                        let randomPhotoImdex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                        let photoDictionary = photoArray[randomPhotoImdex] as? [String: AnyObject]
+                        
+//                        
+                        if let urlStr = photoDictionary![Constants.FlickrResponseKeys.MediumURL] as? String,
+//                            get the url and title from the dictionary chosen at random
+                            let photoTitle = photoDictionary![Constants.FlickrResponseKeys.Title] as? String {
+                            let imageURL = NSURL(string: urlStr)
+//                            if the url returned data then we turn it into NSData format and then send it to the main queue to update the views contents
+                            if let imageData = NSData(contentsOfURL: imageURL!) {
+                                performUIUpdatesOnMain() {
+                                    self.photoImageView.image = UIImage(data: imageData)
+                                    self.photoTitleLabel.text = photoTitle
+                                    self.setUIEnabled(true)
+                                }
+                            }
+                        }
+                    }
+                    
                 }
             }
         }
